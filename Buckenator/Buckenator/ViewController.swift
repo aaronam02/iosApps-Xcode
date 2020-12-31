0f47d4805e90e1f8a949c824d9383115ac5deed6
Aaron Amankwaah <amankwaah123@gmail.com>
2020-12-31 11:36:26 -0500

    day-2 practice/design of Buck App

---
+++ Buckenator/Buckenator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Buckenator
+//
+//  Created by Aaron Amankwaah on 12/31/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
