d284bab9d9cead9c826c451793f1c9f83c944330
Aaron Amankwaah <amankwaah123@gmail.com>
2021-01-02 10:59:09 -0500

    Day-2/

---
+++ Happy Birthday/Happy Birthday/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Happy Birthday
+//
+//  Created by Aaron Amankwaah on 1/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
